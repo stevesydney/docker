8bb7ef923771c5c33a9c1b2910d4eab515231b9b
Steve Farrugia <sfarrugia@gmail.com>
2015-08-05 10:23:12 +1000

    Docker files

---
+++ nginx/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+#For static file on Mac
+sed  -i -e "s|sendfile\s\+on|sendfile off|" /etc/nginx/nginx.conf
+
+sed -i -e "s|%DOCUMENT_ROOT%|${DOCUMENT_ROOT:-/var/www/html}|" /etc/nginx/conf.d/default.conf
+sed -i -e "s|%PHP_FPM%|${PHP_FPM:-php:9000}|" /etc/nginx/conf.d/default.conf
+
+exec "$@"

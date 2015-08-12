acc0bbb7e72e6c55df14032595f4d17a8eb5a2e1
Steve Farrugia <sfarrugia@gmail.com>
2015-08-12 15:13:09 +1000

    Added SERVER_NAME directive to nginx for virtual host name

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -6,5 +6,6 @@ sed  -i -e "s|sendfile\s\+on|sendfile off|" /etc/nginx/nginx.conf
 
 sed -i -e "s|%DOCUMENT_ROOT%|${DOCUMENT_ROOT:-/var/www/html}|" /etc/nginx/conf.d/default.conf
 sed -i -e "s|%PHP_FPM%|${PHP_FPM:-php:9000}|" /etc/nginx/conf.d/default.conf
+sed -i -e "s|%SERVER_NAME%|${SERVER_NAME:-app.dev}|" /etc/nginx/conf.d/default.conf
 
 exec "$@"
